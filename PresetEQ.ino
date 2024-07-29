f3c0a366423eb5fdaedba3fb4e8fec532220ef58
Thomas Brüggemann <mail@thomasbrueggemann.com>
2024-07-29 08:00:00 +0200

    Double tap bypass buttons

--- PresetEQ.ino
+++ PresetEQ.ino
@@ -1,5 +1,3 @@
-
-
 #include <Arduino.h>
 #include "Button2.h"
 
@@ -25,51 +23,68 @@ Button2 footswitch2;
 Button2 footswitch3;
 Button2 footswitch4;
 
-void handleTap(Button2& btn) {
-    if (btn == footswitch1) {
-      Serial.println("1 clicked");
-      setAmpChannelRelay(1);
-      setEQRelay(1);
-    } else if (btn == footswitch2) {
-      Serial.println("2 clicked");
-      setAmpChannelRelay(2);
-      setEQRelay(2);
-    } else if (btn == footswitch3) {
-      Serial.println("3 clicked");
-      setAmpChannelRelay(3);
-      setEQRelay(3);
-    } else if (btn == footswitch4) {
-      Serial.println("4 clicked");
-      setAmpChannelRelay(4);
-      setEQRelay(4);
-    }
+void handleTap(Button2 &btn)
+{
+  if (btn == footswitch1)
+  {
+    Serial.println("1 clicked");
+    setAmpChannelRelay(1);
+    setEQRelay(1);
+  }
+  else if (btn == footswitch2)
+  {
+    Serial.println("2 clicked");
+    setAmpChannelRelay(2);
+    setEQRelay(2);
+  }
+  else if (btn == footswitch3)
+  {
+    Serial.println("3 clicked");
+    setAmpChannelRelay(3);
+    setEQRelay(3);
+  }
+  else if (btn == footswitch4)
+  {
+    Serial.println("4 clicked");
+    setAmpChannelRelay(4);
+    setEQRelay(4);
+  }
 }
 
-void handleDoubleTap() {
+void handleDoubleTap()
+{
   Serial.println("Double tap");
-    digitalWrite(EQ_RELAY_1_PIN, LOW);
-    digitalWrite(EQ_RELAY_2_PIN, LOW);
-    digitalWrite(EQ_RELAY_3_PIN, LOW);
-    digitalWrite(EQ_RELAY_4_PIN, LOW);
+  digitalWrite(EQ_RELAY_1_PIN, LOW);
+  digitalWrite(EQ_RELAY_2_PIN, LOW);
+  digitalWrite(EQ_RELAY_3_PIN, LOW);
+  digitalWrite(EQ_RELAY_4_PIN, LOW);
 }
 
-void setAmpChannelRelay(int footswitch) {
-  if (footswitch == 1) {
+void setAmpChannelRelay(int footswitch)
+{
+  if (footswitch == 1)
+  {
     digitalWrite(AMP_CHANNEL_RELAY_1_PIN, HIGH);
     digitalWrite(AMP_CHANNEL_RELAY_2_PIN, LOW);
     digitalWrite(AMP_CHANNEL_RELAY_3_PIN, LOW);
     digitalWrite(AMP_CHANNEL_RELAY_4_PIN, LOW);
-  } else if (footswitch == 2) {
+  }
+  else if (footswitch == 2)
+  {
     digitalWrite(AMP_CHANNEL_RELAY_1_PIN, LOW);
     digitalWrite(AMP_CHANNEL_RELAY_2_PIN, HIGH);
     digitalWrite(AMP_CHANNEL_RELAY_3_PIN, LOW);
     digitalWrite(AMP_CHANNEL_RELAY_4_PIN, LOW);
-  } else if (footswitch == 3) {
+  }
+  else if (footswitch == 3)
+  {
     digitalWrite(AMP_CHANNEL_RELAY_1_PIN, LOW);
     digitalWrite(AMP_CHANNEL_RELAY_2_PIN, LOW);
     digitalWrite(AMP_CHANNEL_RELAY_3_PIN, HIGH);
     digitalWrite(AMP_CHANNEL_RELAY_4_PIN, LOW);
-  } else if (footswitch == 4) {
+  }
+  else if (footswitch == 4)
+  {
     digitalWrite(AMP_CHANNEL_RELAY_1_PIN, LOW);
     digitalWrite(AMP_CHANNEL_RELAY_2_PIN, LOW);
     digitalWrite(AMP_CHANNEL_RELAY_3_PIN, LOW);
@@ -77,23 +92,31 @@ void setAmpChannelRelay(int footswitch) {
   }
 }
 
-void setEQRelay(int footswitch) {
-  if (footswitch == 1) {
+void setEQRelay(int footswitch)
+{
+  if (footswitch == 1)
+  {
     digitalWrite(EQ_RELAY_1_PIN, HIGH);
     digitalWrite(EQ_RELAY_2_PIN, LOW);
     digitalWrite(EQ_RELAY_3_PIN, LOW);
     digitalWrite(EQ_RELAY_4_PIN, LOW);
-  } else if (footswitch == 2) {
+  }
+  else if (footswitch == 2)
+  {
     digitalWrite(EQ_RELAY_1_PIN, LOW);
     digitalWrite(EQ_RELAY_2_PIN, HIGH);
     digitalWrite(EQ_RELAY_3_PIN, LOW);
     digitalWrite(EQ_RELAY_4_PIN, LOW);
-  } else if (footswitch == 3) {
+  }
+  else if (footswitch == 3)
+  {
     digitalWrite(EQ_RELAY_1_PIN, LOW);
     digitalWrite(EQ_RELAY_2_PIN, LOW);
     digitalWrite(EQ_RELAY_3_PIN, HIGH);
     digitalWrite(EQ_RELAY_4_PIN, LOW);
-  } else if (footswitch == 4) {
+  }
+  else if (footswitch == 4)
+  {
     digitalWrite(EQ_RELAY_1_PIN, LOW);
     digitalWrite(EQ_RELAY_2_PIN, LOW);
     digitalWrite(EQ_RELAY_3_PIN, LOW);
@@ -103,29 +126,29 @@ void setEQRelay(int footswitch) {
 
 void setup()
 {
-	Serial.begin(BAUD_RATE);
+  Serial.begin(BAUD_RATE);
 
-	footswitch1.begin(FOOTSWITCH_1_PIN);
+  footswitch1.begin(FOOTSWITCH_1_PIN);
   footswitch1.setTapHandler(handleTap);
   footswitch1.setDoubleClickHandler(handleDoubleTap);
 
-	footswitch2.begin(FOOTSWITCH_2_PIN);
+  footswitch2.begin(FOOTSWITCH_2_PIN);
   footswitch2.setTapHandler(handleTap);
   footswitch2.setDoubleClickHandler(handleDoubleTap);
 
-	footswitch3.begin(FOOTSWITCH_3_PIN);
+  footswitch3.begin(FOOTSWITCH_3_PIN);
   footswitch3.setTapHandler(handleTap);
   footswitch3.setDoubleClickHandler(handleDoubleTap);
 
-	footswitch4.begin(FOOTSWITCH_4_PIN);
+  footswitch4.begin(FOOTSWITCH_4_PIN);
   footswitch4.setTapHandler(handleTap);
   footswitch4.setDoubleClickHandler(handleDoubleTap);
 }
 
 void loop()
 {
-	footswitch1.loop();
-	footswitch2.loop();
-	footswitch3.loop();
-	footswitch4.loop();
+  footswitch1.loop();
+  footswitch2.loop();
+  footswitch3.loop();
+  footswitch4.loop();
 }
